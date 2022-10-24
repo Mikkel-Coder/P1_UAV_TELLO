1ddad9bfb60a34d24981aeaa6d31f92abb2ff34c
Mikkel-coder <ms22@student.aau.dk>
2022-10-24 11:47:13 +0200

    add kill switch

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from djitellopy import Tello
-from time import sleep
+import atexit, time
+
 
 tello = Tello()
 
+
+def kill_switch():
+    """Kills the drone at exit"""
+    while tello.send_control_command("emergency") == False: # Internal method with return
+        pass
+
+
+atexit.register(kill_switch)
+
+
 tello.connect()
-print(tello.get_battery())
-
 tello.takeoff()
-sleep(1)
-tello.land()
-
-# lav en kill switch (kig på python event. noget med script-end agtigt. Søg på det)
-# sæt altid propeller på på den rigtige måde. Se appen for hjælp.
-
-# download appen.
-
-# lav struktur der kan generere ruter, så man kan smide et område ind, og så laver den selv en rute.
+print('tryk nu!!')
+time.sleep(8)
