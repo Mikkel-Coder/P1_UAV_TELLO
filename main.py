9928faa4b0c7341fd8aedc0df59e9734217ca2b7
Mikkel-coder_GAMING <mikk804s@eucnordvest.dk>
2022-10-25 23:05:15 +0200

    kill_switch now in dev.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from djitellopy import Tello
 import atexit, time
-
+from dev import tools
 
 tello = Tello()
 
+tools.kill_switch(tello)
+atexit.register(tools.kill_switch, self = tello)
 
-def kill_switch():
-    """Kills the drone at exit if it is still flying """
-    if tello.is_flying == True:
-        while tello.send_control_command("emergency") == False: # Internal method with return
-            pass
-        tello.is_flying = False # So that the API ends and the program quits
-atexit.register(kill_switch)
 
 
 tello.connect()
