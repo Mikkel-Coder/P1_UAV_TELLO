af1e6df9e8530319b3cd0855333d052a6209693c
rune2976 <rlund22@student.aau.dk>
2022-12-02 10:29:39 +0100

    koden som virker til gps

--- GPS_class.py
+++ GPS_class.py
@@ -4,36 +4,36 @@ from machine import Pin, UART
 import utime, time
 
 class GPS_Class:
+
+    def __init__(self,):
     
     #Store GPS Coordinates
-    latitude = ""
-    longitude = ""
-    satellites = ""
-    gpsTime = ""
+        self.latitude = ""
+        self.longitude = ""
+        self.satellites = ""
+        self.gpsTime = ""
     #store the status of satellite is fixed or not
-    FIX_STATUS = False
+        self.FIX_STATUS = False
     #Used to Store NMEA Sentences
-    buff = bytearray(255)
-    TIMEOUT = False
+        self.buff = bytearray(255)
+        self.TIMEOUT = False
     #GPS Module UART Connection
-    gps_module = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
+        self.gps_module = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
     
     #function to get gps Coordinates
-    def getPositionData(self,gps_module):
-        global FIX_STATUS, TIMEOUT, latitude, longitude, satellites, gpsTime
+    def getPositionData(self):
         #run while loop to get gps data
         #or terminate while loop after 5 seconds timeout
-        timeout = time.time() + 100   # 8 seconds from now
+        timeout = time.time() + 10   # 8 seconds from now
         while True:
-            gps_module.readline()
-            buff = str(gps_module.readline())
+            self.gps_module.readline()
+            self.buff = str(self.gps_module.readline())
             #parse $GPGGA term
             #b'$GPGGA,094840.000,2941.8543,N,07232.5745,E,1,09,0.9,102.1,M,0.0,M,,*6C\r\n'
-            print(buff)
-            parts = buff.split(',')
+            parts = self.buff.split(',')
             
             #if no gps displayed remove "and len(parts) == 15" from below if condition
-            if (parts[0] == "b'$GPGGA" and len(parts) == 15):
+            if (parts[0] == "b'$GPGGA"):
                 if(parts[1] and parts[2] and parts[3] and parts[4] and parts[5] and parts[6] and parts[7]):
                     #print("Message ID  : " + parts[0])
                     #print("UTC time    : " + parts[1])
@@ -44,21 +44,22 @@ class GPS_Class:
                     #print("Position Fix: " + parts[6])
                     #print("n sat       : " + parts[7])
                     
-                    latitude = self.convertToDigree(parts[2])
+                    self.latitude = self.convertToDigree(parts[2])
                     # parts[3] contain 'N' or 'S'
                     if (parts[3] == 'S'):
-                        latitude = -latitude
-                    longitude = self.convertToDigree(parts[4])
+                        self.latitude = -self.latitude
+                    self.longitude = self.convertToDigree(parts[4])
                     # parts[5] contain 'E' or 'W'
                     if (parts[5] == 'W'):
-                        longitude = -longitude
-                    satellites = parts[7]
-                    gpsTime = parts[1][0:2] + ":" + parts[1][2:4] + ":" + parts[1][4:6]
-                    FIX_STATUS = True
+                        self.longitude = -self.longitude
+                    self.satellites = parts[7]
+                    self.gpsTime = parts[1][0:2] + ":" + parts[1][2:4] + ":" + parts[1][4:6]
+                    self.FIX_STATUS = True
                     break
                     
+                    
             if (time.time() > timeout):
-                TIMEOUT = True
+                self.TIMEOUT = True
                 break
             utime.sleep_ms(500)
             
@@ -75,14 +76,18 @@ class GPS_Class:
         return str(Converted)
         
     def getcoords(self):
-        self.getPositionData(self.gps_module)
+        self.getPositionData()
 
         #if gps data is found then print it on lcd
-        if(FIX_STATUS == True):
-            FIX_STATUS = False
-            return latitude, longitude
+        if(self.FIX_STATUS == True):
+            self.FIX_STATUS = False
+            return self.latitude, self.longitude
             
-        if(TIMEOUT == True):
-            TIMEOUT = False
+        if(self.TIMEOUT == True):
+            self.TIMEOUT = False
             return False
 
+if __name__ == "__main__":
+    getgps=GPS_Class()
+    print(getgps.getcoords())
+    print(getgps.getcoords())
