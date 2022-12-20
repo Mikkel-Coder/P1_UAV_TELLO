3676bfe20741f41f2c2be00841dc52627bd56f7c
Juhlemanden14 <marcusjuhlandersson@gmail.com>
2022-12-20 12:54:14 +0100

    Delete dev.py

--- dev.py
+++
@@ -1,9 +0,0 @@
-class Tools:
-    """Various tools to use when developing the Tello drone."""
-
-    def kill_switch(self):
-        """Stops the drone in a emergency."""
-        if self.is_flying == True:
-            while self.send_control_command("emergency") == False: # Keep sending emergency until accepted
-                pass
-            self.is_flying = False  # Set API variable to False to quit program.
